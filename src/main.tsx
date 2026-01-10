f95a916787542fba18a1e3c98fa5ded895bf8edd
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-01-10 13:43:54 -0700

    Set up React Router with 404 page route
    Co-authored-by: ibrahim <ibrahim.aahmed07@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './components/App'
+import NotFound from './components/NotFound'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/github" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
