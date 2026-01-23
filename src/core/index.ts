d3e9997acb7cdc8300d38b58e1e79d8aad317fd2
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-01-22 21:42:49 -0700

    Fix exports: remove move, avoid parse conflict

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,9 @@
-export * from './regex'
+// Export regex types but NOT parse (cachedAlgorithms exports cached parse)
+export * from './regex/ast'
+export * from './regex/tokenizer'
 export * from './automata'
 // Export algorithms except those that have cached versions
-export { lambdaClosure, move } from './algorithms/lambda'
+export { lambdaClosure } from './algorithms/lambda'
 export { simulateNFA, simulateDFA } from './algorithms/simulate'
 export type { SimulationResult, SimulationStep } from './algorithms/simulate'
 export * from './cache'
