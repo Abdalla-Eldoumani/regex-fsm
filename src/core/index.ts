c592a722cee1fd131eb32817df110b18443fbeb4
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-01-22 21:37:52 -0700

    Fix export conflicts between algorithms and cachedAlgorithms

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,9 @@
 export * from './regex'
 export * from './automata'
-export * from './algorithms'
+// Export algorithms except those that have cached versions
+export { lambdaClosure, move } from './algorithms/lambda'
+export { simulateNFA, simulateDFA } from './algorithms/simulate'
+export type { SimulationResult, SimulationStep } from './algorithms/simulate'
 export * from './cache'
+// Export cached versions of parse, buildNFA, nfaToDFA, minimizeDFA
 export * from './cachedAlgorithms'
