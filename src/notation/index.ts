2f49cf786567bdbbafa151a0932595233657f336
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-06-14 23:32:45 -0600

    re-export NotationProvider, NotationContext, useNotation from notation index

--- src/notation/index.ts
+++ src/notation/index.ts
@@ -1,2 +1,4 @@
 export { formatRegex } from './format'
 export { GLYPHS, type NotationMode } from './glyphs'
+export { NotationProvider, NotationContext, type NotationContextValue } from './NotationContext'
+export { useNotation, type UseNotationReturn } from './useNotation'
