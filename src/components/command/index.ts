fba3f24aac27cfd6d092f574bef10dc9c51944b1
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-06-16 03:25:50 -0600

    export the command palette and registry

---
+++ src/components/command/index.ts
@@ -0,0 +1,8 @@
+export type { Command } from './commandRegistry'
+export { buildNavigationCommands, buildGlobalCommands } from './commandRegistry'
+export {
+  CommandPalette,
+  CommandPaletteProvider,
+  PaletteOpenButton,
+  useCommandPalette,
+} from './CommandPalette'
